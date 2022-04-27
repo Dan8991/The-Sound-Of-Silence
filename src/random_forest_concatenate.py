47bcb13172683f445ed8cfe27bd7919423532855
Federica <federica.latora@studenti.unipd.it>
2022-04-27 09:23:17 +0200

    added random forest classifier on concatenated features

---
+++ src/random_forest_concatenate.py
@@ -0,0 +1,58 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_curve, auc, accuracy_score
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+
+    # importing the data
+    data = pd.read_csv("datasets/df_jsut_multiband_melgan.csv")
+
+    # converting column data to list
+    mse = data['mse'].tolist()
+    kl = data['kl'].tolist()
+    reny = data['reny'].tolist()
+    tsallis = data['tsallis'].tolist()
+    labels = data['labels'].tolist()
+
+    # concatenate the divergences
+    measures = np.column_stack((mse,kl,reny,tsallis)) # shape (num_samples, 4)
+
+    # Shuffle = false since the data has been already shuffled in the .csv file
+    X_train, X_test, y_train, y_test = train_test_split(measures, labels, shuffle=False)
+
+    clf = RandomForestClassifier()
+
+    # Train the model using the training sets
+    clf.fit(X_train, y_train)
+
+    y_pred = clf.predict(X_test)
+    print("Accuracy:", accuracy_score(y_test, y_pred))
+
+    # Predict the classes on the test data, and return the probabilities for each class
+    y_proba = clf.predict_proba(X_test)
+
+    # Plot ROC curve as in: https://sachinkalsi.github.io/blog/category/ml/2018/08/20/top-8-performance-metrics-one-should-know.html#receiver-operating-characteristic-curve-roc
+    # For each of the probability scores(y_proba) if y_proba >= threshold, then predicted label would be positive(1).
+
+    fpr, tpr, th = roc_curve(y_test, y_proba[:, 1], pos_label=1)  # label = 1 = 'Generated' is the positive class
+
+    # AUC is the area under the ROC curve. More the area under the curve, the more good is the model
+    roc_auc = auc(fpr, tpr)
+
+    plt.figure()
+    plt.plot(fpr, tpr, color='darkorange', lw=2, label='ROC curve (area = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], color='navy', lw=2, linestyle='--')
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic (ROC)')
+    plt.legend(loc="lower right")
+    plt.show()
+
+
+
